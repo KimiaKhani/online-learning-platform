06f7d338361a3f77a4fdb2d90d5ab0377b55117e
Kimia <kimia.khani1382@gmail.com>
2025-07-27 12:39:03 +0330

    correct folder

---
+++ backend/routers/router_link.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Depends
+from schema import CourseBase, CourseDisplay
+from sqlalchemy.orm import Session
+from DB.database import get_db
+from DB import db_course
+from authentication import auth
+import logging
+from DB.models import Course
+from fastapi.exceptions import HTTPException
+from DB.db_link import add_link, get_link_for_students, remove_link_by_teacher
+from typing import List, Optional
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+
+router = APIRouter(prefix='/course', tags=['course'])
+
+
+@router.post("/course/{course_id}/add-meet-link")
+def add_google_meet_link_router(course_id: int, meet_link: str, teacher_id: str, db: Session = Depends(get_db)):
+    return add_link(course_id, meet_link, teacher_id, db)
+
+
+
+@router.get("/course/{course_id}/meet-link")
+def get_google_meet_link_for_students_router(course_id: int, student_id: int, db: Session = Depends(get_db)):
+    return get_link_for_students(course_id, student_id, db)
+
+
+
+@router.delete("/course/{course_id}/remove-meet-link")
+def remove_google_meet_link_router(course_id: int, teacher_id: str, db: Session = Depends(get_db)):
+    return remove_link_by_teacher(course_id, teacher_id, db)
